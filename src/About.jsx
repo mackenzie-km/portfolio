cf82157e8b5b500d497c060d89f9f11b9af6ed84
Mackenzie Gonzales <github email address>
2026-02-16 23:44:51 +0900

    fix motion framer timing for bottom paragraphs

--- src/About.jsx
+++ src/About.jsx
@@ -11,7 +11,7 @@ export default function About() {
       <div ref={ref} id="about" className="flex flex-col gap-16 md:flex-row">
         <div className="flex-auto items-center order-2 text-left md:order-1 relative md:min-h-fit">
           <div className="max-w-[1100px] mx-auto px-8 lg:px-6 py-20">
-            <div class="flex flex-column my-8 gap-4">
+            <div class="flex flex-col md:flex-row my-8 gap-4">
               <motion.div
                 initial={{ opacity: 0.3 }}
                 whileInView={{ opacity: 1 }}
@@ -22,13 +22,13 @@ export default function About() {
                 Hi 👋! I’m Mackenzie (but you can call me Mack). I create
                 software that helps people do their best work.
               </motion.div>
-              <div class="flex items-center justify-center order-1 md:order-2">
+              <div class="flex items-center justify-center order-1 md:order-2 pt-8 md:pt-0">
                 <div
                   class="relative rounded-3xl
            shadow-[0_20px_40px_rgba(0,0,0,0.08)]  bg-gradient-to-br from-[var(--color-accent-dark)] to-[var(--color-accent-med)] opacity-100 py-2 px-2 mt-2 md:mt-0"
                 >
                   <img
-                    class="rounded-3xl min-h-[50px] min-w-[50px] max-w-[100px] max-h-[100px] md:max-w-[150px] md:max-h-[150px]"
+                    class="rounded-3xl min-h-[100px] min-w-[100px] max-w-[250px] md:max-w-[150px] md:max-h-[150px]"
                     id="profile-photo"
                     src={headshot}
                     alt="Profile Photo"
@@ -40,11 +40,11 @@ export default function About() {
           <section className="relative pt-8 pb-16">
             <div className="absolute inset-0 bg-[#f8f5f2] h-auto -z-10" />
             <div className="absolute inset-0 bg-white top-auto  -z-10" />
-            <div className="max-w-6xl mx-auto text-center px-16 p-8">
-              <h2 className="text-2xl md:text-2xl font-semibold font-[var(--font-nunito)] pb-8">
+            <div className="max-w-6xl mx-auto text-center px-16 py-16">
+              <h2 className="text-3xl md:text-4xl font-semibold font-[var(--font-nunito)] pb-16">
                 About Me:
               </h2>
-              <div className="grid gap-8 md:grid-cols-3">
+              <div className="grid gap-8 pb-4 md:grid-cols-3">
                 <SpecialtyCard
                   icon="</>"
                   title="Coding"
@@ -63,51 +63,30 @@ export default function About() {
                   description="I have a passion for cooking and share recipes inspired by family and international friends."
                 />
               </div>
-            </div>
-          </section>
-          <section className="relative pt-8 pb-16  bg-[#f1ece7]">
-            <div className="max-w-3xl mx-auto px-16 p-8">
-              <h2 className="text-2xl md:text-2xl font-semibold font-[var(--font-nunito)] text-center pt-4">
-                My Story:
-              </h2>
               <motion.div
                 initial={{ opacity: 0.3 }}
                 whileInView={{ opacity: 1 }}
                 viewport={{ once: false, amount: 0.5 }}
                 transition={{ duration: 1.5 }}
-                class="mt-8 mb-8 max-w-3xl mx-auto text-[1.05rem] leading-relaxed text-neutral-700 font-[var(--font-nunito)]"
               >
-                I majored in Animal Science @ UC Davis and spent several years
-                working in biotech. While there, I began automating my own work
-                with Excel and SQL, and learned how to reduce errors and free up
-                my time for more important work. That discovery led me back to
-                school in 2019 to study software engineering, and I’ve been
-                working as an engineer since 2020.
-              </motion.div>
-              <motion.div
-                initial={{ opacity: 0.3 }}
-                whileInView={{ opacity: 1 }}
-                viewport={{ once: false, amount: 0.5 }}
-                transition={{ duration: 1.5 }}
-                class="mt-8 mb-8 max-w-3xl mx-auto text-[1.05rem] leading-relaxed text-neutral-700 font-[var(--font-nunito)]"
-              >
-                For me, the "who" always comes before the "what." I care deeply
-                about my teammates and the people we’re building for. Tech
-                stacks, tools, and coding preferences matter, but what I really
-                get excited about is working alongside others to help others.
-                This is why I've always loved the mentoring aspects of software
-                engineering. And this is also one reason why I mentor
-                international students outside of work.
-              </motion.div>
-              <motion.div
-                initial={{ opacity: 0.3 }}
-                whileInView={{ opacity: 1 }}
-                viewport={{ once: false, amount: 0.5 }}
-                transition={{ duration: 1.5 }}
-                class="mt-8 max-w-3xl mx-auto text-[1.05rem] leading-relaxed text-neutral-700 font-[var(--font-nunito)]"
-              >
-                Feel free to explore my projects, writing, and more about me.
-                Let’s build something together!
+                <p class="mt-8 mb-8 max-w-3xl mx-auto text-[1.05rem] leading-relaxed text-neutral-700 font-[var(--font-nunito)]">
+                  I studied Animal Science at UC Davis and spent several years
+                  in biotech, where I began automating my work with Excel and
+                  SQL to reduce errors and focus on higher-impact problems. That
+                  experience led me back to school in 2019 to study software
+                  engineering, and I’ve been working as an engineer since 2019.
+                </p>
+                <p class="mt-8 mb-8 max-w-3xl mx-auto text-[1.05rem] leading-relaxed text-neutral-700 font-[var(--font-nunito)]">
+                  For me, the “who” comes before the “what.” I care deeply about
+                  my teammates and the people we’re building for. Beyond the
+                  tech stack, I’m motivated by collaboration, mentorship, and
+                  building things that genuinely help others—including mentoring
+                  international students outside of work.
+                </p>
+                <p class="mt-8 max-w-3xl mx-auto text-[1.05rem] leading-relaxed text-neutral-700 font-[var(--font-nunito)]">
+                  Feel free to explore my projects, writing, and more about me.
+                  Let’s build something together!
+                </p>
               </motion.div>
             </div>
           </section>
