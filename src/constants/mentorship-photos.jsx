62cb08af64c7428ef0d5e3435c1fa13f9bee3a03
mackenzie-km <mackenzie.gonzales.k@gmail.com>
2026-04-13 19:40:08 +0900

    update photos for mentorship section

--- src/constants/mentorship-photos.jsx
+++ src/constants/mentorship-photos.jsx
@@ -1,15 +1,15 @@
 import parallax1 from "../assets/mentorship/parallax-1.jpg";
-import parallax2 from "../assets/mentorship/parallax-2.png";
-import parallax3 from "../assets/mentorship/parallax-3.jpg";
+import parallax2 from "../assets/mentorship/parallax-2.jpg";
+import parallax3 from "../assets/mentorship/parallax-3.png";
 import parallax4 from "../assets/mentorship/parallax-4.png";
 import parallax5 from "../assets/mentorship/parallax-5.jpg";
-import parallax6 from "../assets/mentorship/parallax-6.png";
+import parallax6 from "../assets/mentorship/parallax-6.jpg";
 
 export const imageMap = {
-  1: { path: parallax1, text: "Interview Advice: Workshop" },
-  2: { path: parallax2, text: "Chicago: Workshop" },
-  3: { path: parallax3, text: "Tokyo: Student Reunion" },
-  4: { path: parallax4, text: "Tokyo: Student Reunion" },
-  5: { path: parallax5, text: "Tokyo: Cultural Event" },
-  6: { path: parallax6, text: "Tokyo: Cultural Event" },
+  1: { path: parallax1, text: "Evanston: Interview Advice Workshop" },
+  2: { path: parallax2, text: "Evanston: Cultural Workshop" },
+  3: { path: parallax3, text: "Tokyo: STEM Panel Speaker" },
+  4: { path: parallax4, text: "Tokyo: English Event Coordinator" },
+  5: { path: parallax5, text: "Evanston: Intl Event Coordinator" },
+  6: { path: parallax6, text: "Chicago: Outreach Coordinator" },
 };
